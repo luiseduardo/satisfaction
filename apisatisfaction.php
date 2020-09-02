2acaec5ef20aa09872693b672a3223ccf75d409c
Luis Eduardo <luis.eduardo@gmail..com>
2020-09-01 22:21:42 -0300

    fix: add field comment

--- apisatisfaction.php
+++ apisatisfaction.php
@@ -48,20 +48,65 @@ if (!$track->getFromDB($ticket_satisfaction->ticket_id)) {
     exit;
 }
 
-if (($track->fields['status'] == Ticket::CLOSED) && $satisfaction->getFromDB($ticket_satisfaction->ticket_id)) {
-    if (!is_null($satisfaction->fields['date_answered'])) {
-        echo "<p class='center b'>" . __('Pesquisa já respondida!') . "</p>";
-        exit;
+if ($_POST) {
+
+    if ($satisfaction->getFromDB($ticket_satisfaction->ticket_id)) {
+        if (!is_null($satisfaction->fields['comment'])) {
+            echo "<p class='center b'>" . __('Pesquisa já respondida!') . "</p>";
+            exit;
+        }
     }
+
+    $satisfaction->getFromDB($ticket_satisfaction->ticket_id);
+    $satisfaction->update($_POST);
+
+    echo "<p class='center b'>" . __('Seus comentários foram adicionados a pesquisa!') . "</p>";
 } else {
-    echo "<p class='center b'>" . __('No generated survey') . "</p>";
-    exit;
+
+    // save satisfaction
+    if ($satisfaction->getFromDB($ticket_satisfaction->ticket_id)) {
+        if (!is_null($satisfaction->fields['date_answered'])) {
+            $data = [];
+            $data['satisfaction'] = $ticket_satisfaction->star;
+            $data['tickets_id'] = $ticket_satisfaction->ticket_id;
+            $satisfaction->getFromDB($ticket_satisfaction->ticket_id);
+            $satisfaction->update($data);
+        }
+    }
+?>
+    <div id="page">
+        <form name="form" method="post" action="<?php echo $CFG_GLPI["url_base"] ?>/plugins/satisfaction/apisatisfaction.php?t=<?php echo $_GET['t'] ?>" enctype="multipart/form-data">
+            <input type="hidden" name="plugin_satisfaction_surveys_id" value="<?php echo $ticket_satisfaction->satisfaction_id ?>">
+            <input type="hidden" name="tickets_id" value="<?php echo $ticket_satisfaction->ticket_id ?>">
+            <div class="spaced" id="tabsbody">
+                <table class="tab_cadre_pager">
+                    <tbody>
+                        <tr class="tab_bg_2">
+                            <td class="b big"><span class="status"><i class="itilstatus fas fa-circle closed" title="Fechado"></i></span>Ticket <?php echo $ticket_satisfaction->ticket_id ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table class="tab_cadre_fixe" id="mainformtable">
+                    <tbody>
+                        <tr class="headerRow">
+                            <th colspan="2" class="">
+                                Pesquisa de Satisfação<br>
+                                <small>Pesquisa respondida com sucesso! caso julgue necessário informe seu comentário abaixo e clique em "Salvar"</small>
+                            </th>
+                        </tr>
+                        <tr class="tab_bg_2">
+                            <td>Comentários</td>
+                            <td><textarea cols="45" rows="7" name="comment"></textarea></td>
+                        </tr>
+                        <tr>
+                            <td class="center" colspan="2"><input type="submit" value="Salvar" name="update" class="submit"></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <?php
+            Html::closeForm();
+            ?>
+    </div>
+<?php
 }
-
-$data = [];
-$data['satisfaction'] = $ticket_satisfaction->star;
-$data['tickets_id'] = $ticket_satisfaction->ticket_id;
-$satisfaction->getFromDB($ticket_satisfaction->ticket_id);
-$satisfaction->update($data);
-
-echo "<p class='center b'>" . __('Pesquisa respondida com sucessoo!') . "</p>";
